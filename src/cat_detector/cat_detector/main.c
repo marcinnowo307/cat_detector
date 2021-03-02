d88bbaeac6e07ebc681d9583a60aa1c7765674df
marcinnowo307 <43822211+marcinnowo307@users.noreply.github.com>
2021-03-02 17:25:37 +0100

    added a software reset option, to reset the device after opening the door
    it got tidious to click the reset button every time I wanted to turn off the alarm. If the door gets opened, the cat can get in anyways, so there is no need to make the user click the button.

--- src/cat_detector/cat_detector/main.c
+++ src/cat_detector/cat_detector/main.c
@@ -8,6 +8,7 @@
 
 #include <avr/io.h>
 #include <avr/sleep.h>
+#include <avr/wdt.h>
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 #include <util/delay.h>
@@ -22,6 +23,7 @@ typedef struct {
 	uint8_t blinks_in_one_rep;
 	uint8_t should_led_stay_on;
 	uint8_t should_buzz_every_time;
+	uint8_t should_reset_on_door_opened;
 } configuration;
 
 void goToSleep();
@@ -33,6 +35,8 @@ void setBuzzerPwm(configuration * conf);
 // determines if the cat is standing behind the door, returns 1 if it is, 0 otherwise
 #define isTheCatWaiting() (isPirOn() && isReedOn())
 
+
+
 // R/Wr EEProm configuration variables
 void writeConf(configuration * conf);
 void readConf(configuration * conf);
@@ -42,6 +46,16 @@ uint8_t eeprom_is_fresh();
 // fills the struct with defaults
 void set_default_config(configuration * conf);
 
+
+
+// resets the device using the watchdog timer, see also wdt_off
+void fireSoftwareReset();
+// turns off the watchdog timer during the initialization see avr-libc documentation on the watchdog
+void wdt_off() __attribute__((naked)) __attribute__((section(".init3")));
+
+// after calling this function, if the door gets opened, the device will reset itself
+#define armOpenDoorReset() (PCMSK |= (1 << PCINT4))
+#define resetConditionsMet() (isReedOff() && (PCMSK & (1 << PCINT4)))
 
 
 int main(void)
@@ -66,7 +80,7 @@ int main(void)
 	setBuzzerPwm(&conf);
 	
 	
-	PRR  |= (1 << PRADC); // disable adc
+	PRR  |= (1 << PRADC); // disable adc. Do not disable timer/counter 0, as it is used for pwm
 	ACSR |= (1 << ACD);   // disable analog comparator
 	PORTB |= (1 << PORTB0); // PB0 is floating, so pullup to save power
 	
@@ -79,23 +93,39 @@ int main(void)
     while (1) 
     {
 		goToSleep();
+		
 		if(isTheCatWaiting())
 		{
 			playAlarmSequence(&conf);
+			
+			// it's redundant to do these checks on every alarm, but it is easier that way
 			if(conf.should_led_stay_on)
 				ledOn();
-		}
-		else
-		{
-			//ledOff();
+			if(conf.should_reset_on_door_opened)
+				armOpenDoorReset();
 		}
 	}
 }
 
 
+void fireSoftwareReset()
+{
+	wdt_enable(WDTO_15MS);
+	while(1);
+}
+
+void wdt_off()
+{
+	MCUSR = 0;
+	wdt_disable();
+
+	return;
+}
+
+
 void goToSleep()
 {
-	PRR |= (1 << PRTIM0);
+	PRR |= (1 << PRTIM0); // timer 0 used for pwm
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	cli();
 	sleep_enable();
@@ -111,7 +141,6 @@ void setBuzzerPwm(configuration * conf)
 {
 	DDRB |= (1 << DDB1);
 	TCCR0A = (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
-	//TCCR0B |= (1 << WGM02);
 	OCR0B  = conf->loudness;
 }
 
@@ -146,13 +175,14 @@ void playAlarmSequence(configuration * conf)
 
 ISR(PCINT0_vect)
 {
-
+	if(resetConditionsMet())
+		fireSoftwareReset();
 }
 
 // returns 1 if the eeprom needs to be configured with defaults
-uint8_t eeprom_is_fresh()
+inline uint8_t eeprom_is_fresh()
 {
-	return (eeprom_read_byte((uint8_t *)5) != 0);
+	return (eeprom_read_byte((uint8_t *)6) != 0);
 }
 
 void writeConf(configuration * conf)
@@ -162,7 +192,8 @@ void writeConf(configuration * conf)
 	eeprom_update_byte((uint8_t *)2,conf->blinks_in_one_rep);
 	eeprom_update_byte((uint8_t *)3,conf->should_led_stay_on);
 	eeprom_update_byte((uint8_t *)4,conf->should_buzz_every_time);
-	eeprom_update_byte((uint8_t *)5,0); // set a flag which signifies, that the eeprom is configured
+	eeprom_update_byte((uint8_t *)5,conf->should_reset_on_door_opened);
+	eeprom_update_byte((uint8_t *)6,0); // set a flag which signifies, that the eeprom is configured
 }
 
 
@@ -173,6 +204,7 @@ void readConf(configuration * conf)
 	conf->blinks_in_one_rep = eeprom_read_byte((uint8_t *)2);
 	conf->should_led_stay_on = eeprom_read_byte((uint8_t *)3);
 	conf->should_buzz_every_time = eeprom_read_byte((uint8_t *)4);
+	conf->should_reset_on_door_opened = eeprom_read_byte((uint8_t *)5);
 }
 
 void set_default_config(configuration * conf)
@@ -182,4 +214,5 @@ void set_default_config(configuration * conf)
 	conf->loudness = 4;
 	conf->should_led_stay_on = 1;
 	conf->should_buzz_every_time = 0;
+	conf->should_reset_on_door_opened = 1;
 }
